cca9623ef0d65e8fe77b50e3b2e96e0d894c1a8f
KnasBas <anton.jansson@elev.ga.ntig.se>
2019-10-08 12:53:03 +0200

    första commit

---
+++ Slagsmål.Klasser/Slagsmål.Klasser/Fighter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slagsmål.Klasser
+{
+    class Fighter
+    {
+        private int hp = 100;
+
+        public string name = "";
+
+        public void Attack()
+        {
+
+        }
+
+        public void Hurt(int ammount)
+        {
+
+        }
+
+        public bool isAlive()
+        {
+
+
+            return true; //tillfällig
+        }
+        
+        public int GetHp()
+        {
+
+
+
+            return hp;
+        }
+    }
+}
---
+++ Slagsmål.Klasser/Slagsmål.Klasser/Figther.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slagsmål.Klasser
+{
+    class Figther
+    {
+        private int hp = 100;
+
+        public string name = "";
+
+
+        public void Attack() //int metod
+        {
+
+        }
+
+    }
+}
